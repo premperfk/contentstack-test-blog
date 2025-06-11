d208c5d7d0f95f81b1f8ae4785fe5782bc465ea8
Premcharan Kalla <premcharan.kalla@perficient.com>
2025-06-11 09:14:08 +0530

    Update contentstack location

--- src/contentstackConfig.js
+++ src/contentstackConfig.js
@@ -2,8 +2,10 @@ import * as contentstack from 'contentstack';
 
 const Stack = contentstack.Stack({
   api_key: 'blt87a38a6427588401',
-  delivery_token: 'cs558dec6ca5dfefcd7786a790',
+  delivery_token: 'csd2121e1f2e28c42dc0e4a56f',
   environment: 'development',
+  region: contentstack.Region.AZURE_EU,
+  host: 'azure-eu-cdn.contentstack.com'
 });
 
 export default Stack;
